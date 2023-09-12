16d0a149fa23584bc82604020e53cfaf03abd865
gtz.jesus <gtz.jesus@outlook.com>
2023-09-12 13:30:35 -0600

    Authentication: User Login with Supabase

--- 17-the-wild-oasis/final-5-after-sort-filter-pagination/src/features/authentication/LoginForm.jsx
+++ 17-the-wild-oasis/final-5-after-sort-filter-pagination/src/features/authentication/LoginForm.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+import { useState } from 'react';
+import Button from '../../ui/Button';
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
+import { useLogin } from './useLogin';
+import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('jesus@a.com');
+  const [password, setPassword] = useState('pass123');
+  const { login, isLoading } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!email || !password) return;
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -20,6 +27,7 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRow>
       <FormRow label="Password" orientation="vertical">
@@ -29,10 +37,13 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRow>
       <FormRow orientation="vertical">
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoading}>
+          {!isLoading ? 'Log in' : <SpinnerMini />}
+        </Button>
       </FormRow>
     </Form>
   );
