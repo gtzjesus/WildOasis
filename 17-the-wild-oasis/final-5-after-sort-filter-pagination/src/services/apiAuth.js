d2b16b76a63c93bee3df951d3ff13b2ceccca72b
gtz.jesus <gtz.jesus@outlook.com>
2023-09-12 14:06:15 -0600

    Logout Implementation

--- 17-the-wild-oasis/final-5-after-sort-filter-pagination/src/services/apiAuth.js
+++ 17-the-wild-oasis/final-5-after-sort-filter-pagination/src/services/apiAuth.js
@@ -1,14 +1,4 @@
 import supabase from './supabase';
-export async function login({ email, password }) {
-  let { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
-
-  if (error) throw new Error(error.message);
-  console.log(data);
-  return data;
-}
 
 export async function getCurrentUser() {
   const { data: session } = await supabase.auth.getSession();
@@ -16,6 +6,21 @@ export async function getCurrentUser() {
 
   const { data, error } = await supabase.auth.getUser();
   if (error) throw new Error(error.message);
-  console.log(data);
   return data?.user;
+}
+
+export async function login({ email, password }) {
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw new Error(error.message);
+  return data;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) throw new Error(error.message);
 }
