0407e747e7070b7d1c590093cfcbe455b514b52c
gtz.jesus <gtz.jesus@outlook.com>
2023-09-14 14:02:08 -0600

    User Sign Up

--- 17-the-wild-oasis/final-5-after-sort-filter-pagination/src/services/apiAuth.js
+++ 17-the-wild-oasis/final-5-after-sort-filter-pagination/src/services/apiAuth.js
@@ -1,5 +1,21 @@
 import supabase from './supabase';
 
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: '',
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+  return data;
+}
+
 export async function getCurrentUser() {
   const { data: session } = await supabase.auth.getSession();
   if (!session.session) return null;
