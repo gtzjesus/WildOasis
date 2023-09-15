dfefea5e2f4a7f77b903d6bdfaaeb582386037db
gtz.jesus <gtz.jesus@outlook.com>
2023-09-15 16:06:14 -0600

    Displaying a Pie Chart

--- 17-the-wild-oasis/final-5-after-sort-filter-pagination/src/features/dashboard/DashboardLayout.jsx
+++ 17-the-wild-oasis/final-5-after-sort-filter-pagination/src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from './useRecentStays';
 import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -42,8 +43,7 @@ function DashboardLayout() {
 
         <div>Statistics</div>
         <div>Today's activity </div>
-        <div>Chart stay durations</div>
-        <div>Chart sales</div>
+        <DurationChart confirmedStays={confirmedStays} />
         <SalesChart bookings={bookings} numDays={numDays} />
       </StyledDashboardLayout>
     </>
