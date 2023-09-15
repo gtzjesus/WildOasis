914752d580eebd3b47322ded2640691ce8c169b4
gtz.jesus <gtz.jesus@outlook.com>
2023-09-15 14:17:31 -0600

    Implementing Dark Mode

--- 17-the-wild-oasis/final-5-after-sort-filter-pagination/src/features/authentication/useUpdateUser.js
+++ 17-the-wild-oasis/final-5-after-sort-filter-pagination/src/features/authentication/useUpdateUser.js
@@ -11,9 +11,9 @@ export function useUpdateUser() {
       toast.success('User account successfully updated');
 
       // UPDATE DATA MANUALLY IN CACHE
-      // queryClient.setQueryData('user', user);
+      queryClient.setQueryData(['user'], user);
 
-      queryClient.invalidateQueries({ queryKey: ['user'] });
+      // queryClient.invalidateQueries({ queryKey: ['user'] });
     },
     onError: (err) => toast.error(err.message),
   });
